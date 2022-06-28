d3a4f85f107e1b66a9666fb97d71677c1e07b971
Asher Harvey-Smith <asherharveysmith@gmail.com>
2022-06-28 16:17:00 +0100

    refactor the solution to be a bit nicer

--- problem-3/problem-3.hs
+++ problem-3/problem-3.hs
@@ -2,6 +2,7 @@ import Data.Maybe (fromJust)
 import Data.List (find)
 
 primes :: [Int]
+-- seive of Eratosphenes
 primes = seive [2 ..]
   where
     seive (x : xs) = x : seive (filter (\n -> mod n x /= 0) xs)
@@ -9,11 +10,11 @@ primes = seive [2 ..]
 primeFactors :: Int -> [Int]
 primeFactors n
     | n <= 1 = []
-    | otherwise = fromJust (
-            do
-                p <- find (\x -> mod n x == 0) primes
-                return $ p : primeFactors (div n p)
-        )
+    | otherwise = 
+      let
+        p = fromJust $ find (\x -> mod n x == 0) primes
+      in
+        p : primeFactors (div n p)
 
 main :: IO ()
 main = print $ maximum $ primeFactors 600851475143
