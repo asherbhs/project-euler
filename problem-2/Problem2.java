1ff1bf71291f6fce3cab5c97b6f5a8db1316d560
Asher Harvey-Smith <asherharveysmith@gmail.com>
2022-06-28 04:59:06 +0100

    Create java solution to problem 2 and modify the Java running script to clean up all .class files

---
+++ problem-2/Problem2.java
@@ -0,0 +1,33 @@
+import java.util.stream.IntStream;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        new Problem2().solve();
+    }
+
+    public void solve() {
+        FibonacciGenerator f = new FibonacciGenerator();
+        System.out.println(
+
+            IntStream.generate(
+                () -> f.nextFibonacci()
+            )
+                .takeWhile(n -> n <= 4000000)
+                .filter(n -> n % 2 == 0)
+                .sum()
+
+        );
+    }
+
+    private class FibonacciGenerator {
+        private int fib1 = 1;
+        private int fib2 = 2;
+
+        public int nextFibonacci() {
+            int hold = fib1;
+            fib1 = fib2;
+            fib2 = fib2 + hold;
+            return hold;
+        }
+    }
+}
