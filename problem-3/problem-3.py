6baae2192d338d18853f5444a983ef39f8017f28
Asher Harvey-Smith <asherharveysmith@gmail.com>
2022-06-28 18:03:13 +0100

    Python solution to problem 3 (works in theory but reaches recursion limit for target)

---
+++ problem-3/problem-3.py
@@ -0,0 +1,31 @@
+# this works but for the large number you reach the recursion limit
+# python is just not made for super funtional recursive stuff :(
+
+# from typing import Generator
+
+# def nats(n: int) -> Generator[int, None, None]:
+#     yield n
+#     yield from nats(n + 1)
+
+# def seive(ns: Generator[int, None, None]) -> Generator[int, None, None]:
+#     p: int = next(ns)
+#     yield p
+#     yield from seive(filter(
+#         lambda x: x % p != 0,
+#         ns
+#     ))
+
+# def primes() -> Generator[int, None, None]: return seive(nats(2))
+
+# def prime_factors(n: int) -> list[int]:
+#     if n <= 1: 
+#         return []
+
+#     p: int = next(x for x in primes() if n % x == 0)
+#     ps: list[int] = prime_factors(n / p)
+#     ps.insert(0, p)
+#     return ps
+
+# print(prime_factors(600851475143)[-1])
+
+
