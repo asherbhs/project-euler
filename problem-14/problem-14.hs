a0d210f1554dd1669d47353c27aaba9511488f66
Asher Harvey-Smith <asherharveysmith@gmail.com>
2022-06-30 17:47:00 +0100

    Speed up the solution by pre-calculating the Collatz lengths

--- problem-14/problem-14.hs
+++ problem-14/problem-14.hs
@@ -3,9 +3,16 @@
 import Data.List (maximumBy)
 import Data.Ord (comparing)
 
+infixr 1 ?
+(?) :: Bool -> a -> a -> a
+(?) p x y = if p then x else y
+
 collatz :: Int -> [Int]
 collatz 1 = [1]
-collatz n = n : collatz (if even n then div n 2 else 3 * n + 1)
+collatz n = n : collatz (even n ? div n 2 $ 3 * n + 1)
 
 main :: IO ()
-main = print $ maximumBy (comparing (length . collatz)) [1 .. 1_000_000]
+main = print 
+    $ fst
+    $ maximumBy (comparing snd) 
+    $ map (\n -> (n, length $ collatz n)) [1 .. 1_000_000]
