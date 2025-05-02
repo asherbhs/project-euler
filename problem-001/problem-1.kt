ef289c8224a7bb738981ef40dea9d28dcb593829
Asher Harvey-Smith <asherharveysmith@gmail.com>
2025-05-02 12:23:16 +0100

    apparently I never comitted these

--- problem-001/problem-1.kt
+++ problem-001/problem-1.kt
@@ -1,7 +1,5 @@
 fun main() = println(
-
     (1 until 1000)
         .filter { it % 3 == 0 || it % 5 == 0 }
         .sum()
-
 )
--- problem-002/problem-2.hs
+++ problem-002/problem-2.hs
@@ -6,7 +6,7 @@ iterate2 :: (a -> a -> a) -> a -> a -> [a]
 iterate2 f x y = x : iterate2 f y (f x y)
 
 main :: IO ()
-main = print $ 
+main = print $
     fibonacci
         & takeWhile (<= 4_000_000)
         & filter even
